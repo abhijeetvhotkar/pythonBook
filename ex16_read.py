25eac7123e9f5dc7ed9cc7736bfc5366eff1dc49
abhi <abhijeetvhotkar@gmail.com>
2018-02-24 14:33:53 -0500

    Adding more exercises

--- ex16_read.py
+++ ex16_read.py
@@ -1,9 +1,3 @@
-from sys import argv
-
-script, filename = argv
-
-txt = open(filename)
-
-print(f"Here's your file {filename}:")
-print(txt.read())
-txt.close()
+adasfgsdgs 
+dfasfasdgsdfgsd 
+fasfsdgsdgasd 
---
+++ ex21.py
@@ -0,0 +1,31 @@
+def add(a, b):
+    print(f"Adding {a} + {b}")
+    return a + b
+
+def subtract(a, b):
+    print(f"Subtracting {a} - {b}")
+    return a - b
+
+def multiply(a, b):
+    print(f"Multiplying {a} * {b}")
+    return a * b
+
+def divide(a, b):
+    print(f"Dividing {a} / {b}")
+    return a / b
+
+print ("Let's do some math with just fuctions!")
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}")
+
+# A puzzle for the extra credit type it in any way
+print("Here is a puzzle.")
+
+what = multiply(age, subtract(height, add(weight, divide(iq, 2))))
+
+print("That becomes: ", what, "Can you do it by hand?")
